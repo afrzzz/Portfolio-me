7f25a8bcdf5aad8533cd638a76acf3d0e38e79c9
afrzzz <afrizalardiansyah2001@gmail.com>
2025-09-05 18:56:22 +0700

    Update Bug Fixes

--- vite.config.ts
+++ vite.config.ts
@@ -1,33 +1,23 @@
-import tailwindcss from '@tailwindcss/vite'
-import react from '@vitejs/plugin-react'
-import laravel from 'laravel-vite-plugin'
-import { defineConfig } from 'vite'
-
-const isVercel = process.env.VERCEL === '1'
-
-const plugins = [
-  laravel({
-    input: ['resources/css/app.css', 'resources/js/app.tsx'],
-    ssr: 'resources/js/ssr.tsx',
-    refresh: true,
-  }),
-  react(),
-  tailwindcss(),
-]
-
-// ⛔ jangan import wayfinder di atas
-if (!isVercel) {
-  const { wayfinder } = require('@laravel/vite-plugin-wayfinder')
-  plugins.push(
-    wayfinder({
-      formVariants: true,
-    })
-  )
-}
+import { wayfinder } from '@laravel/vite-plugin-wayfinder';
+import tailwindcss from '@tailwindcss/vite';
+import react from '@vitejs/plugin-react';
+import laravel from 'laravel-vite-plugin';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
-  plugins,
-  esbuild: {
-    jsx: 'automatic',
-  },
-})
+    plugins: [
+        laravel({
+            input: ['resources/css/app.css', 'resources/js/app.tsx'],
+            ssr: 'resources/js/ssr.tsx',
+            refresh: true,
+        }),
+        react(),
+        tailwindcss(),
+        wayfinder({
+            formVariants: true,
+        }),
+    ],
+    esbuild: {
+        jsx: 'automatic',
+    },
+});
